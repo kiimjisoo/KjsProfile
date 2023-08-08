b08c1e56a757641e7c7fcf61ab94241097b1ccb4
kiimjisoo <jasoo22@naver.com>
2023-08-08 16:28:26 +0900

    0808
    안 쓴느 이미지 삭제
    import 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import { Route, Routes } from 'react-router';
 import Header from './components/Header';
-import MainVideo from './components/MainVideo';
 import Info from './components/Info';
 import Skill from './components/Skill';
 import Project from './components/Project';
@@ -30,17 +27,10 @@ function App() {
           
       <Header />
       <Info />  
-      {/* <MainVideo /> */}
       <Career />
       <Skill />
       <Project />
       <Contact />
-
-      {/* <Routes>
-        <Route path='/' element={<Header />}>
-        <Route element={<MainVideo />}/>
-        </Route>
-      </Routes> */}
     </>
   );
 }
